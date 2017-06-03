b686fdb212eb1b5a0e84678a007d7074588b1d4e
takeshi <takeshi.farro@gmail.com>
2017-06-02 23:37:38 -0500

    migrations for create tables

--- database/migrations/2017_05_28_001324_create_table_animes.php
+++ database/migrations/2017_05_28_001324_create_table_animes.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTableAnimes extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::dropIfExists('users');
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('animes', function(Blueprint $table){
             $table->increments('id');
-            $table->string('name', 100);
-            $table->string('email', 100)->unique();
-            $table->string('password', 100);
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('name');
+            $table->string('synopsis');
+            $table->double('valuation', 4, 2);
+
         });
     }
 
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('animes');
     }
 }
