3dcb60ee0d14ac5a6c167125956fb66b0a6a3800
takeshifh <takeshi.farro@gmail.com>
2017-08-08 21:28:12 -0500

    seiyuus table created

---
+++ database/migrations/2017_08_09_021727_create_table_seiyuus.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableSeiyuus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seiyuss', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('last_name', 100);
+            $table->string('first_name', 100);
+            $table->string('img', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seiyuss');
+    }
+}
