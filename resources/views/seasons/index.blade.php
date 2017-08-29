1ecd0460006e8efe1cbc66562cde59bd0a4fc087
takeshifh <takeshi.farro@gmail.com>
2017-08-28 22:10:15 -0500

    index of season improvement

--- resources/views/seasons/index.blade.php
+++ resources/views/seasons/index.blade.php
@@ -2,28 +2,28 @@
  
 @section('content')
 
-    <div class="content-box big-box">
-      <div class="table-responsive">
-        <table class="table">
-          <thead>
-            <th>Name</th>
-            <th>Start Date</th>
-            <th>End Date</th>
-            <th>Year</th>
-            <th>Actions</th>
-          </thead>
-          <tbody>
-            @foreach($seasons as $season)
-              <tr>
-                <td>{{ $season->name }}</td>
-                <td>{{ $season->start_date }}</td>
-                <td>{{ $season->end_date }}</td>
-                <td>{{ $season->year }}</td>
-                <td></td>
-              </tr>
-            @endforeach
-          </tbody>
-        </table>
-      </div>
-    </div>
+<div class="content-box big-box">
+  <div class="table-responsive">
+    <table class="table">
+      <thead>
+        <th>Name</th>
+        <th>Start Date</th>
+        <th>End Date</th>
+        <th>Year</th>
+        <th>Actions</th>
+      </thead>
+      <tbody>
+        @foreach($seasons as $season)
+          <tr>
+            <td>{{ $season->name }}</td>
+            <td>{{ $season->start_date }}</td>
+            <td>{{ $season->end_date }}</td>
+            <td>{{ $season->year }}</td>
+            <td></td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
 @endsection
