9978613487c9baef1429bc0433c52a3511b56260
takeshifh <takeshi.farro@gmail.com>
2017-08-30 18:20:33 -0500

    reset email blade updated with the base layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,15 @@
  
 @section('content')
 <div class="container">
- <div class="row">
- <div class="col-md-10 col-md-offset-1">
- <div class="panel panel-default">
- <div class="panel-heading">Home</div>
- 
- <div class="panel-body">
- You are using bootstrap
- </div>
- </div>
- </div>
- </div>
+  <div class="row">
+    <div class="col-md-10 col-md-offset-1">
+      <div class="panel panel-default">
+        <div class="panel-heading">Home</div>
+        <div class="panel-body">
+          Welcome to ANIMEZ-ONE!
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
