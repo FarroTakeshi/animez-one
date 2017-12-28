dd3ac5a934c22dba5380128f3fe745b66ce66ed5
FarroTakeshi <takeshi.farro@gmail.com>
2017-12-28 01:53:50 -0500

    delete img of langs and team and add laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,7 +9,10 @@ const { mix } = require('laravel-mix');
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
- */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+js('resources/assets/js/app.js', 'public/js')
+ */
+mix.copyDirectory('resources/assets/img', 'public/img');
+   .sass('resources/assets/sass/style.scss', 'public/css')
+   //.sass('resources/assets/sass/*.scss', 'public/css')
+   .sourceMaps();
