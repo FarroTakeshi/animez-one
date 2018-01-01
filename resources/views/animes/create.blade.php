b8eea8ee40511592961aeefeff5cf6d765cdbd38
FarroTakeshi <takeshi.farro@gmail.com>
2017-12-31 22:19:28 -0500

    update create anime blade and add jasny js and css

--- resources/views/animes/create.blade.php
+++ resources/views/animes/create.blade.php
@@ -52,6 +52,15 @@
       @endforeach
     </div>
 
+    <div class="fileinput fileinput-new input-group" data-provides="fileinput">
+      <span class="input-group-addon btn btn-default btn-file"><span class="fileinput-new"><i class="fa fa-file"></i> Choose file</span><span class="fileinput-exists">Change</span>
+        <input type="hidden"><input type="file" name="...">
+      </span>
+      <div class="form-control" data-trigger="fileinput"><span class="fileinput-filename"></span>
+      </div>
+      <a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
+    </div>
+
     <button type="submit" class="btn btn-primary"><i class="fa fa-fw fa-upload"></i>
       <span>Register</span>
     </button>
@@ -59,3 +68,11 @@
     </form>
 </div>
 @endsection
+
+@push('extra-css')
+<link rel="stylesheet" href="{{ asset('css/jasny-bootstrap/dist/css/jasny-bootstrap.min.css') }}" type="text/css" >
+@endpush
+
+@push('extra-js')
+<script src="{{ asset('js/jasny-bootstrap/dist/js/jasny-bootstrap.min.js') }}"></script>
+@endpush
